29c8eb20ce394dd56c4f8dbcd741a1de5ec5466d
yube01 <114811769+yube01@users.noreply.github.com>
2023-01-06 19:51:58 +0545

    navbar style added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,9 +7,42 @@ import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import EmailIcon from '@mui/icons-material/Email';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import { Link } from 'react-router-dom';
+
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <div className="navbar">
+      <div className="left">
+        <Link to = "/" style={{textDecoration:"none"}}>
+        <span>
+          BeSocial.
+        </span>
+        </Link>
+       
+        <HomeIcon/>
+        <NightlightIcon/>
+        <GridViewIcon/>
+        <div className="search">
+        <SearchIcon/>
+          <input type="text" placeholder='Search' />
+
+        </div>
+        
+        
+
+      </div>
+      <div className="right">
+        <PersonIcon/>
+        <EmailIcon/>
+        <NotificationsIcon/>
+        <div className="person">
+          <img src="" alt="" />
+          <span>
+            John Curry
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
